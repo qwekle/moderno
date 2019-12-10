e82cc7f94579eef223f1eea3632e17ca5e53b983
qwekle <qwop1606@gmail.com>
2019-12-10 14:57:50 +0300

    added adaptive

--- app/js/main.js
+++ app/js/main.js
@@ -11,6 +11,32 @@ $(function () {
         arrows: false,
         slidesToShow: 4,
         slidesToScroll: 4,
+        responsive: [
+            {
+                breakpoint: 1900,
+                settings: {
+                    slidesToShow: 3,
+                    slidesToScroll: 3,
+                    infinite: true,
+                    dots: true
+                }
+            },
+            {
+                breakpoint: 1441,
+                settings: {
+                    slidesToShow: 2,
+                    slidesToScroll: 2,
+                }
+            },
+            {
+                breakpoint: 801,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: false,
+                }
+            },
+        ]
     });
 
     $(".js-range-slider").ionRangeSlider({
